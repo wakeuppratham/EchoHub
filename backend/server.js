545c070c86950485b23d1d67262a4e2d3160c558
Pratham <prathamg2003@gmail.com>
2024-02-04 14:28:40 +0530

    Update server.js and package.json configurations

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,30 @@
 const express = require("express");
-const dotenv = require("dotenv");
-const {chats} =  require("./data/data");
+const { chats } = require("./data/data");
 
 const app = express();
-dotenv.config();
+require("dotenv").config();
 
-app.get("/",(req,res)=>{
+
+app.get("/", (req, res) => {
     res.send("API is Running Successfully");
 });
 
-app.get("/api/chat",(req,res)=>{
+app.get("/api/chat", (req, res) => {
     res.send(chats);
 });
 
-app.get("/api/chat/:id",(req,res)=>{
+app.get("/api/chat/:id", (req, res) => {
     // console.log(req.params.id);
-    const singleChat = chats.find((c)=>c._id === req.params.id);
+    const singleChat = chats.find((c) => c._id === req.params.id);
     res.send(singleChat);
-})
+});
 
 const PORT = process.env.PORT || 5000;
 
-
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`Server started on PORT ${PORT}`);
 });
+
+//connect to DB
+const connectDB  = require("./config/db");
+connectDB();
