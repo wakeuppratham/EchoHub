e7179a83a632763df85d2e18af41aa4dfef774bf
pulkit-20 <pulkit2469.be21@chitkara.edu.in>
2024-02-06 21:21:45 +0530

    HomePage ui

--- frontend/src/Pages/Homepage.js
+++ frontend/src/Pages/Homepage.js
@@ -1,11 +1,52 @@
-import React from 'react';
+import React from "react";
+import {
+  Tabs,
+  Center,
+  Box,
+  Container,
+  Tab,
+  TabList,
+  TabPanels,
+  TabPanel,
+} from "@chakra-ui/react";
+import Signup from "../components/authentication/Signup";
+import Login from "../components/authentication/Login";
 
 const Homepage = () => {
-    return (
-        <div>
-            Home
-        </div>
-    );
+  return (
+    <Container maxW="xl" centerContent>
+      <Box
+        d="flex"
+        justifyContent="center"
+        p={3}
+        bg={"white"}
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Center fontSize="4xl" fontFamily="work-sans" color="black">
+          EchoHub
+        </Center>
+      </Box>
+      <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
+        <Tabs variant="soft-rounded" colorScheme="purple">
+          <TabList mb="1em">
+            <Tab width="50%">Login</Tab>
+            <Tab width="50%">Sign Up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Login />
+            </TabPanel>
+            <TabPanel>
+              <Signup />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  );
 };
 
 export default Homepage;
