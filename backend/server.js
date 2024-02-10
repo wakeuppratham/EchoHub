0e77e62871df218b59ed2cb2dc232651ed014ddd
pulkit-20 <pulkit2469.be21@chitkara.edu.in>
2024-02-11 04:22:35 +0530

    userAuthentication using jwt tokens

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,25 @@
 const express = require("express");
 const { chats } = require("./data/data");
 const dotenv = require("dotenv");
+const userRoutes = require("./routes/userRoutes");
+const connectDB = require("./config/db");
 
-
-const app = express();
 dotenv.config();
+connectDB();
+const app = express();
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("API is running");
 });
 
-app.use
+app.use("/api/user", userRoutes);
 
+app.user(notFound)
+app.user(errorHandler)
 
 //using port number from .env file
 const PORT = process.env.PORT || 5000;
-
 
 app.listen(PORT, console.log(`Server added on PORT ${PORT}`));
