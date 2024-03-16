d991ba68fea90f4749c9f5594fe6bc155654a690
Pratham <prathamg2003@gmail.com>
2024-03-16 22:18:22 +0530

    Minor changes in user controllers file

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -44,7 +44,7 @@ const registerUser = asyncHandler(async(req,res)=>{
 });
 
 const authUser = asyncHandler(async (req, res) => {
-    const { email, password } = req.body;
+    // const { email, password } = req.body;
     console.log(req.body);
 
     const user = await User.findOne({ email });
