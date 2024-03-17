3952fbe603ac5935a821fd659af62b10643f2df6
Pratham <prathamg2003@gmail.com>
2024-03-17 13:50:30 +0530

    Undo mistaken changes

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -44,8 +44,8 @@ const registerUser = asyncHandler(async(req,res)=>{
 });
 
 const authUser = asyncHandler(async (req, res) => {
-    // const { email, password } = req.body;
-    console.log(req.body);
+    const { email, password } = req.body;
+    // console.log(req.body);
 
     const user = await User.findOne({ email });
 
